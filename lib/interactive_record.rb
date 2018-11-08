ad43fa089d5f4040100220abf6ada173dd05878c
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-08 21:29:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,15 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    sql = "pragma table_info(#{table_name})"
+
+    info = DB[:conn].execute(sql)
+    column_names = []
+    info.each do |row|
+      column_names << row[:name]
+    end
+    column_names.compact
+  end
+
 end
